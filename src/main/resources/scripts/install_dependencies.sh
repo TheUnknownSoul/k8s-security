b8a2700a4c12c860745de69dab217805292547de
unknown-soul <andriyroy3@gmail.com>
2025-08-06 08:35:38 -0400

    Small fix in install_dependencies.sh :bug: Update dependencies

--- src/main/resources/scripts/install_dependencies.sh
+++ src/main/resources/scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a binary_list=("unzip" "trivy" "kubectl" "openjdk-17-jdk" "python3" "cvemap")
+declare -a binary_list=("unzip" "trivy" "kubectl" "java" "python3" "cvemap")
 
 declare -a deb_soft=("unzip" "openjdk-17-jdk" "python3")
 
@@ -9,8 +9,6 @@ function install_trivy() {
   cd "$directory" || echo "Can't change installation directory" && exit 1
   curl -LO https://github.com/aquasecurity/trivy/releases/download/v0.63.0/trivy_0.63.0_Linux-64bit.deb -o trivy.deb
   sudo dpkg --install trivy*.deb
-  cd /tmp || exit 1
-  find /tmp -delete -type d -name "installer*"
 }
 
 function install_kubectl() {
@@ -19,8 +17,6 @@ function install_kubectl() {
   curl -LO https://dl.k8s.io/release/"$(curl -LS https://dl.k8s.io/release/stable.txt)"/bin/linux/amd64/kubectl
   chmod +x ./kubectl
   sudo mv ./kubectl /usr/local/bin/kubectl
-  cd /tmp || exit 1
-  find /tmp -delete -type d -name "installer*"
 }
 
 function install_cvemap() {
@@ -29,14 +25,12 @@ function install_cvemap() {
   curl -LO https://github.com/projectdiscovery/cvemap/releases/download/v0.0.7/cvemap_0.0.7_linux_amd64.zip
   unzip cvemap*.zip
   sudo mv ./cvemap /usr/local/bin/cvemap
-  cd /tmp || exit 1
-  find /tmp -delete -type d -name "installer*"
 }
 
 function check(){
   binary=$1
   if [[ -n $(which "$binary") ]]; then
-    echo "$binary installed"
+    echo "[+] $binary installed"
   else
     echo "$binary not installed. Installing..."
     if [[ $(echo "${deb_soft[@]}" | grep -ow "$binary" | wc -w) -eq "1" ]]; then
